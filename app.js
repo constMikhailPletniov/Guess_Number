38ea4e3dc3af82151b90d146d8688497369e4a41
Vladislav <vlad120686@gmail.com>
2021-10-31 22:54:39 +0200

    review f random

--- app.js
+++ app.js
@@ -1,29 +1,12 @@
-const count = document.querySelector('#attempts'); //из инпута
-const maxValue = document.querySelector('#max-value'); //из инпута
-const minValue = document.querySelector('#min-value'); //из инпута
+const startPage = document.querySelector('.start-page');
+const gamePage = document.querySelector('.game-page');
 
 const maxValue = 10;
-const minValue = 2;
+const minValue = 5;
 
 function random(minValue, maxValue) {
-  if (
-    typeof maxValue === 'number' &&
-    typeof minValue === 'number' &&
-    minValue > 0 &&
-    minValue < maxValue &&
-    maxValue < 201 &&
-    minValue === minValue &&
-    maxValue === maxValue
-  ) {
-    let count =
-      Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
-    console.log(count);
-    return count;
-  } else {
-    let count =
-      Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
-    console.log(count);
-    return count;
+  if (typeof maxValue === 'number' && typeof minValue === 'number' && minValue > 0 && minValue < maxValue && maxValue < 201 && minValue === minValue && maxValue === maxValue) {
+    return Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
   }
 }
 random(minValue, maxValue);

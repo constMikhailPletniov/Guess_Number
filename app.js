55f4461ae4c6a79a9849907f019b15d6bfb03bb2
Vladislav <vlad120686@gmail.com>
2021-11-03 21:21:57 +0200

    delete all let

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const startPage = document.querySelector('.start-page');
-const gamePage = document.querySelector('.game-page');
-
-const maxValue = 10;
-const minValue = 5;
-
 function random(minValue, maxValue) {
-  if (typeof maxValue === 'number' && typeof minValue === 'number' && minValue > 0 && minValue < maxValue && maxValue < 201 && minValue === minValue && maxValue === maxValue) {
+  if (
+    typeof maxValue === 'number' &&
+    typeof minValue === 'number' &&
+    minValue > 0 &&
+    minValue < maxValue &&
+    maxValue < 201 &&
+    minValue === minValue &&
+    maxValue === maxValue &&
+    minValue !== maxValue
+  ) {
     return Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
   }
 }

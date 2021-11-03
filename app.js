7448aa35d2447f1be9be8c2250766f30b58509b4
constMikhailPletniov <mihailpl1995@gmail.com>
2021-11-03 22:33:38 +0200

    Changed selectors

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ let maxValue = 0;
 let attempts = 0;
 let getRandomValue = 0;
 
-document.querySelector('.btn-start').addEventListener('click', () => {
+document.querySelector('#btn-start').addEventListener('click', () => {
 
-    minValue = Number(document.querySelector('#min-number').value);
-    maxValue = Number(document.querySelector('#max-number').value);
+    minValue = Number(document.querySelector('#minValue').value);
+    maxValue = Number(document.querySelector('#maxValue').value);
     attempts = Number(document.querySelector('#attempts').value);
     getRandomValue = random(minValue, maxValue);
     staticAttemptsValue = attempts;
@@ -15,7 +15,8 @@ document.querySelector('.btn-start').addEventListener('click', () => {
 
     document.querySelector('.start-page').style.display = 'none';
     document.querySelector('.game-page').style.display = 'flex';
-    document.querySelector('.greetings').textContent = `Привет, я загадал число от ${minValue} до ${maxValue} вашего диапазона. Попробуй угадать его за ${attempts} попыток!`;
+    document.querySelector('.greetings').textContent = `Привет, я загадал число от ${minValue} до ${maxValue} вашего диапазона.`;
+    document.querySelector('.total-number-of-attempts').textcontent = `Попробуй угадать его за ${attempts} попыток!`;
     return minValue, maxValue, attempts, getRandomValue, staticAttemptsValue;
 });
 
@@ -44,6 +45,7 @@ document.querySelector('.btn-generate').addEventListener('click', () => {
         case getRandomValue === userValue:
             document.querySelector('.out').textContent = `Поздравляю! Ты угадал задуманное число за ${count} попыток`;
             document.querySelector('.btn-generate').disabled = true;
+            document.querySelector('.img').display = 'block';
             break;
         case (count >= 2 && getRandomValue !== userValue):
             prevNum.push(userValue);
@@ -53,10 +55,12 @@ document.querySelector('.btn-generate').addEventListener('click', () => {
             switch (true) {
 
                 case (Math.abs(prevDistance) > Math.abs(userValueDistance)):
-                    document.querySelector('.out').textContent = `Не угадал, но теплее!!! Осталось ${attempts} попыток`;
+                    document.querySelector('.out').textContent = `Не угадал, но теплее!!!`;
+                    document.querySelector('.remaining-attempts').textContent = `Осталось ${attempts} попыток`;
                     break;
                 case (Math.abs(prevDistance) < Math.abs(userValueDistance)):
-                    document.querySelector('.out').textContent = `Не угадал, холоднее… Осталось ${attempts} попыток`;
+                    document.querySelector('.out').textContent = `Не угадал, холоднее…`;
+                    document.querySelector('.remaining-attempts').textContent = `Осталось ${attempts} попыток`;
                     break;
             }
             break;
@@ -71,17 +75,21 @@ document.querySelector('.btn-exit').addEventListener('click', () => {
     attempts = 0;
     getRandomValue = 0;
     count = 0;
-    prevNum = [];
+    prevNum.length = 0;
     prevDistance = 0;
     userValueDistance = 0;
 
     document.querySelector('.btn-generate').disabled = false;
-    document.querySelector('#min-number').value = '1';
-    document.querySelector('#max-number').value = '2';
+    document.querySelector('#minValue').value = '1';
+    document.querySelector('#maxValue').value = '2';
     document.querySelector('#attempts').value = '1';
     document.querySelector('#userValue').value = "";
     document.querySelector('.out').textContent = "";
+    document.querySelector('.remaining-attempts').textContent = "";
     document.querySelector('.start-page').style.display = 'flex';
     document.querySelector('.game-page').style.display = 'none';
+    document.querySelector('.img').display = 'none';
+    document.querySelector('.total-number-of-attempts').textcontent = "";
+    document.querySelector('.greetings').textContent = "";
 });
 

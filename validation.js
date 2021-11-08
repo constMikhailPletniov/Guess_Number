8fd0ea4f0e446d899be6e14d943e50f5a616ce78
Vladislav <vlad120686@gmail.com>
2021-11-08 23:57:03 +0200

    chanche validation

--- validation.js
+++ validation.js
@@ -1,26 +1,37 @@
-let textError = document.getElementById('#error');
-//let textError2 = document.getElementById('#error');
+let blocksTextError = document.querySelectorAll('.error'); // Вынести в domObjects.js - доступ к блоку error для вывода сообщения
 
-userValueminValue.addEventListener('onchange', validationGame);
-minValue.addEventListener('onchange', validationStart);
-maxValue.addEventListener('onchange', validationStart);
+dom.inputMin.addEventListener('input', validationInput);
+dom.inputMax.addEventListener('input', validationInput);
+dom.inputAttempts.addEventListener('input', validationInput);
+dom.inputUser.addEventListener('input', validationInput);
+
+function validationInput() {
+  console.log(Number(this.value));
+  //Проверки >0 && целое ли число.
+  if (Number(this.value) > 0 && isInteger(Number(this.value))) {
+    //или (Number(this.value) ^ 0) === Number(this.value);
+    document.querySelector('#btn-start').disabled = false;
+    document.querySelector('.btn-generate').disabled = false;
+  } else {
+    document.querySelector('#btn-start').disabled = true;
+    document.querySelector('.btn-generate').disabled = true;
+    inputWarningText();
+  }
+}
 
 function validationStart() {
-  if (Number(dom.inputMin.value) < Number(dom.inputMax.value) && isInteger(dom.inputMin.value) && isInteger(dom.inputMax.value)) {
+  if (Number(dom.inputMin.value) < Number(dom.inputMax.value)) {
     ButtonStart(); // function call START
   } else {
-    textError.style.color = '#f00';
-    textError.innerText = 'Enter correct value!';
+    blocksTextError[1].innerText = 'Please, enter correct values!';
+    blocksTextError[1].style.color = '#f00';
     setTimeout(buttonExit, 1000); //reset - function clear
   }
 }
 
-function validationGame() {
-  if (isInteger(dom.userValue.value) && dom.inputMax.value > 0) {
-    ButtonStart(); // function call START
-  } else {
-    textError2.style.color = '#f00';
-    textError2.innerText = 'Enter correct value!';
-    setTimeout(buttonExit, 1000); //reset - function clear
+function inputWarningText() {
+  for (el of blocksTextError) {
+    el.textContent = 'Please, enter correct values!';
+    el.style.color = '#f00';
   }
 }

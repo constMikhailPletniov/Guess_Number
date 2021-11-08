e376bf530cb2a5090a020f70ae39d8b0cde7d6ff
Vladislav <vlad120686@gmail.com>
2021-11-09 01:26:51 +0200

    create f validationStart

--- validation.js
+++ validation.js
@@ -4,28 +4,36 @@ dom.inputMin.addEventListener('input', validationInput);
 dom.inputMax.addEventListener('input', validationInput);
 dom.inputAttempts.addEventListener('input', validationInput);
 dom.inputUser.addEventListener('input', validationInput);
+dom.startBtn.addEventListener('click', validationStart);
 
 function validationInput() {
   console.log(Number(this.value));
   //Проверки >0 && целое ли число.
-  if (Number(this.value) > 0 && isInteger(Number(this.value))) {
+  if (Number(this.value) > 0 && (Number(this.value) ^ 0) === Number(this.value)) {
+    console.log('true');
+    console.log(this.value);
     //или (Number(this.value) ^ 0) === Number(this.value);
+    clearWarningText();
     document.querySelector('#btn-start').disabled = false;
     document.querySelector('.btn-generate').disabled = false;
   } else {
+    console.log('false');
+    console.log(this.value);
+    console.log(blocksTextError);
     document.querySelector('#btn-start').disabled = true;
     document.querySelector('.btn-generate').disabled = true;
     inputWarningText();
+    setTimeout(clearWarningText, 2000);
   }
 }
 
 function validationStart() {
   if (Number(dom.inputMin.value) < Number(dom.inputMax.value)) {
-    ButtonStart(); // function call START
+    document.querySelector('#btn-start').disabled = false;
+    buttonStart();
   } else {
-    blocksTextError[1].innerText = 'Please, enter correct values!';
-    blocksTextError[1].style.color = '#f00';
-    setTimeout(buttonExit, 1000); //reset - function clear
+    inputWarningText();
+    setTimeout(clearWarningText, 2000);
   }
 }
 
@@ -33,5 +41,11 @@ function inputWarningText() {
   for (el of blocksTextError) {
     el.textContent = 'Please, enter correct values!';
     el.style.color = '#f00';
+  }
+}
+
+function clearWarningText() {
+  for (el of blocksTextError) {
+    el.textContent = '';
   }
 }

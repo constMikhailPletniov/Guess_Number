4d7f29943e095680db1a79f9f6178d525c5bc6be
olga210600 <89257451+olga210600@users.noreply.github.com>
2021-11-15 00:04:36 +0200

    added second them

---
+++ changeSubject.js
@@ -0,0 +1,12 @@
+const swichMode = document.getElementById('btn-change-subject');
+
+swichMode.onclick = function (){
+    const theme = document.getElementById('theme');
+
+    if (theme.getAttribute('href') === 'style.css'){
+        theme.href = 'yellow-mode.css'
+    }else {
+        theme.href = 'style.css'
+
+    }
+}

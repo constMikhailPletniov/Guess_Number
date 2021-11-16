de32c162131181b4f0ccddb4f71d7ebb7e070968
Vladislav <vlad120686@gmail.com>
2021-11-16 21:11:57 +0200

    review 3

--- script/app.js
+++ script/app.js
@@ -1,4 +1,3 @@
-
 let minValue = 0;
 let maxValue = 0;
 let attempts = 0;
@@ -9,27 +8,27 @@ let count = 0;
 let prevDistance = 0;
 let userValueDistance = 0;
 
-ElementsDom.startBtn.addEventListener('click', validationStart);
+ElementsDom.startBtn.addEventListener('click', validateStart);
 ElementsDom.generateBtn.addEventListener('click', buttonGenerate);
 ElementsDom.exitBtn.addEventListener('click', buttonExit);
-ElementsDom.inputMin.addEventListener('input', validationInput);
-ElementsDom.inputMax.addEventListener('input', validationInput);
-ElementsDom.inputAttempts.addEventListener('input', validationInput);
-ElementsDom.inputUser.addEventListener('input', validationInput);
+ElementsDom.inputMin.addEventListener('input', validateInput);
+ElementsDom.inputMax.addEventListener('input', validateInput);
+ElementsDom.inputAttempts.addEventListener('input', validateInput);
+ElementsDom.inputUser.addEventListener('input', validateInput);
 ElementsDom.btnClear.addEventListener('click', buttonExit);
 ElementsDom.selectLang.addEventListener('change', changeLanguage);
 ElementsDom.questionIcon.addEventListener('mouseover', () => {
-    ElementsDom.instruction.style.display = 'block'
-    ElementsDom.windowElement.style.opacity = '0.3'
+  ElementsDom.instruction.style.display = 'block';
+  ElementsDom.windowElement.style.opacity = '0.3';
 });
 ElementsDom.questionIcon.addEventListener('mouseout', () => {
-    ElementsDom.instruction.style.display = 'none'
-    ElementsDom.windowElement.style.opacity = '1'
+  ElementsDom.instruction.style.display = 'none';
+  ElementsDom.windowElement.style.opacity = '1';
 });
 ElementsDom.switchMode.addEventListener('click', () => {
-    if (ElementsDom.theme.getAttribute('href') === 'style.css') {
-        ElementsDom.theme.href = 'yellow-mode.css';
-    } else {
-        ElementsDom.theme.href = 'style.css';
-    }
+  if (ElementsDom.theme.getAttribute('href') === 'style.css') {
+    ElementsDom.theme.href = 'yellow-mode.css';
+  } else {
+    ElementsDom.theme.href = 'style.css';
+  }
 });
